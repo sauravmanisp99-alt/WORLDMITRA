9faff6df79f202d23f623d598a04af58cd80a8f4
sauravmanisp99-alt <sauravmanisp99@gmail.com>
2026-09-14 21:28:55 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,7 +2,39 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebas
 import { getDatabase } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-database.js";
 
 const firebaseConfig = {
+  apiKey: import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-database.js";
+
+const firebaseConfig = {
   apiKey: "AIzaSyBAhtPH_elsDDkBsxZsvMO8FYwdOGna84g",
+  authDomain: "worldmitra-33ca6.firebaseapp.com",
+  databaseURL: "https://worldmitra-33ca6-default-rtdb.firebaseio.com",
+  projectId: "worldmitra-33ca6",
+  storageBucket: "worldmitra-33ca6.firebasestorage.app",
+  messagingSenderId: "1089520845776",
+  appId: "1:1089520845776:web:befa5a9d2215093734afd5"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+export { app, db };
+  authDomain: "worldmitra-33ca6.firebaseapp.com",
+  databaseURL: "https://worldmitra-33ca6-default-rtdb.firebaseio.com",
+  projectId: "worldmitra-33ca6",
+  storageBucket: "worldmitra-33ca6.firebasestorage.app",
+  messagingSenderId: "1089520845776",
+  appId: "1:1089520845776:web:befa5a9d2215093734afd5"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+export { app, db }
   authDomain: "worldmitra-33ca6.firebaseapp.com",
   databaseURL: "https://worldmitra-33ca6-default-rtdb.firebaseio.com",
   projectId: "worldmitra-33ca6",
